4640e854eeb1f5f7c4c591402b0f2185081f9912
Nikolaas Verlee <nikotomad@gmail.com>
2018-12-13 20:57:04 +0100

    CORS added :)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
+require('./configs/cors.config')
 
 const express = require('express');
 const port = process.env.PORT || 8000;
 const logger = require('morgan');
 const app = express();
+const cors = require('cors');
 const homeRouter = require('./routes/home.routes');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
---
+++ configs/cors.config.js
@@ -0,0 +1,10 @@
+const whitelist = ['http://localhost:8000']
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
