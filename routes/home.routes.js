f40a3aa773b691e03699e7bfe5fb8f38b527e7b7
Nikolaas Verlee <nikotomad@gmail.com>
2018-12-15 15:10:16 +0100

    working filters on server aswell

--- routes/home.routes.js
+++ routes/home.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home.controller');
 
 router.get('/', homeController.greet);
+router.get('/homes/:city/:type', homeController.getSpecificTypeHomes)
 router.get('/homes', homeController.getAllHomes)
 
 module.exports = router;
